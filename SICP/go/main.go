2f6e58099d6bd898ccbe53ff61559f97d14b016e
Serhii <sergey.crisp@gmail.com>
2025-11-24 16:24:56 +0200

    add conditionals evaluation order check in golang

--- SICP/go/main.go
+++ SICP/go/main.go
@@ -5,6 +5,21 @@ import "fmt"
 func main() {
 	// expressionsEvaluation()
 	// a:= if true { 1 } else { 2 }
+	conditionalsEvaluation()
+
+}
+
+func conditionalsEvaluation() {
+	a := false && (func() bool {
+		fmt.Println("evaluating function in and")
+		return true
+	}())
+	b := true || (func() bool {
+		fmt.Println("evaluating function in or")
+		return false
+	}())
+	fmt.Println(a)
+	fmt.Println(b)
 }
 
 func expressionsEvaluation() {
